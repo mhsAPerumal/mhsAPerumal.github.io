537e24ee30b042a772f348e2a5d6166314a7afe9
mhsAPerumal <57224256+mhsAPerumal@users.noreply.github.com>
2019-11-05 10:03:22 -0500

    Add files via upload

---
+++ Snowman/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+  createCanvas(800, 600);
+}
+
+function draw() {
+  // put drawing code here
+rect(5, 300, 400, 100);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+line(160, 200, 40, 40);
+line(240, 200, 400, 50);
+}
