41d5cf485f3246f0cf8b2de8e7a5cf20e80866c0
mhsAPerumal <57224256+mhsAPerumal@users.noreply.github.com>
2019-12-03 19:58:51 -0500

    Update sketch.js

--- aperu1_UsingTheMouse/sketch.js
+++ aperu1_UsingTheMouse/sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  // put setup code here
+  createCanvas(600, 600)
 }
 
 function draw() {
-  // put drawing code here
+}
+
+function mouseMoved() {
+  //This function is called when the mouse is moved
+ clear() // stop the object from leaving trails on the canvas
+  ellipse(mouseX, mouseY, 20, 20) //draw a circle that moves along the canvas
+  line(mouseX, mouseY, 50, 350) /*draw a line from one fixed point to the mouse
+                                  (and an ellipse at the end of it) */
+}
+
+function mouseClicked() {
+  /* This function is called when the mouse is clicked]
+      (but the mouse cannot move at the same time)
+      and add a rectangle next to the ellipse */
+      rect(mouseX, mouseY, 20, 20)
 }
