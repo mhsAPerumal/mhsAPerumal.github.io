1f4ccf83c6286ae7b2befe717ab17a44236390b2
mhsAPerumal <57224256+mhsAPerumal@users.noreply.github.com>
2019-12-20 09:20:25 -0500

    Update sketch.js

--- levelsExample/sketch.js
+++ levelsExample/sketch.js
@@ -16,11 +16,12 @@ let img;
 var ballX = 400;
 var ballY = 50;
 var ballSize = 50;
-var xSpeed = 5;
-var ySpeed = 5;
+var xSpeed = 4;
+var ySpeed = 4;
 
-var alienHeadX = 432
-var alienHeadY = 342
+var time = 30;
+
+var playerHitAlien;
 
 var circleHit;
 function preload() {
@@ -35,8 +36,8 @@ function setup() {
   createCanvas(800,600);
 
   //Assign the initial values for x and y
-  x = width/2;
-  y = height/2;
+  x = random(windowWidth);
+  y = random(windowHeight);
 }
 
 function draw() {
@@ -53,7 +54,7 @@ function draw() {
     text('ALIEN DOMINATION', width/2, height/2);
 
     textSize(16);
-    text('The Glorg alien race, is a species determined to take over the earth. \nUse your arrow keys to use your ball to touch the alien to stop it from getting to earth.', width/2, height/2+50);
+    text('The Glorg alien race, is a species determined to take over the earth. \nUse your arrow keys to use your ball to touch the alien to stop it from getting to earth before time runs out.', width/2, height/2+50);
     text('Press enter to start level one (with each passing level, you get slower and the alien gets faster)', width/2, height/2+100);
 
     //If they press enter, move to the next level (game)
@@ -67,19 +68,35 @@ function draw() {
 
   /*--------------------GAME--------------------*/
   else if (level ===1) {
-
     //Change the background and add the character
-
     background(255);
-    ellipse(alienHeadX,alienHeadY,42)
+    ellipse(x,y,42);
     image(img,x,y);
+    /* The player's time will be at the
+      top left of the screen*/
+      textSize(24);
+      fill(0, 0, 255);
+      text("Time left: " - time, 50, 30);
+      var d = dist(ballX, ballY);
+      if (d > x) {
+        time--; //add a point when the circle is clicked
+      }
+
+    //Shows the lose screen if the time runs out
+    if (time == 0) {
+      clear();
+      background(220);
+      textSize(32);
+      fill(255, 0, 0);
+      text('The Glorg alien have taken over', windowWidth/2, windowHeight/2);
+    }
     //Move the character based on input from the user (arrow keys)
     if (keyIsDown(RIGHT_ARROW)) {
-    ballX = ballX + xSpeed
+    ballX = ballX + xSpeed;
     }
 
     if (keyIsDown(LEFT_ARROW)) {
-      ballX = ballX + -xSpeed
+      ballX = ballX + -xSpeed;
     }
 
     if (keyIsDown(UP_ARROW)) {
@@ -95,18 +112,6 @@ function draw() {
       level+=1
     }
   }
-  /*--------------------END GAME--------------------*/
-
-  /*--------------------WIN SCREEN--------------------*/
-  else if (level===2) {
-    //Change the backround and inform the user that they won
-    background(0,255,0);
-    fill(255);
-    textAlign(CENTER, CENTER);
-    textSize(32);
-    text('YOU WIN!', width/2, height/2);
-    textSize(16);
-    text('Press enter to play again', width/2, height/2+50);
 
     //If the user presses enter, reset the x and y value and reset the level to level 1 (game)
     if (keyIsDown(ENTER)) {
@@ -114,16 +119,13 @@ function draw() {
       y=height/2;
       level=1;
     }
-
-  /*--------------------END WIN SCREEN--------------------*/
 }
 //When the level starts, this draw a ball
-  if (level == 1) {
+if (level == 1) {
   fill(255,0,0);
   ellipse(ballX,ballY,ballSize);
-
-
 }
+
 //This stops the ball from going off the screen
 if (ballX>775) {
   ballX = 775;
@@ -137,15 +139,26 @@ if (ballY>575) {
 if (ballY<25) {
   ballY = 25;
 }
-//shows the win screen if the fifth level has been won
-if (level == 3)
-//Shows the lose screen if the alien touches the edge
-if (alienHeadX>750 || alienHeadX<0) {
-  background(200,0,0);
-  text ('The Glorg alien race have taken over', width/2, height/2);
+//if level two start, you are slower
+if(level == 2) {
+  ballY = ballY - 1;
+  ballX = ballX - 1;
 }
-if (alienHeadY>550 || alienHeadY<0) {
-  background(200,0,0);
-  text ('The Glorg alien race have taken over', width/2, height/2);
+//shows the win screen if the third level is shown
+  if(level == 3) {
+    clear();
+    background(220);
+    textSize(32);
+    fill(255, 255, 255);
+    text('You Win!', windowWidth/2, windowHeight/2);
+  }
 }
+
+hitAlien()
+
+function hitAlien() {
+   playerHitAlien = collideCircleCircle(ballX,ballY,ballSize,x,y,42);
+   if (playerHitAlien == true) {
+     level = level + 1
+   }
 }
